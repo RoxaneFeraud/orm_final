7931a11b9e46df443b9936cb1932fdd2d4d6a30b
Roxane <roxane.feraud@epsi.fr>
2018-07-05 11:24:40 +0200

    Merge origin/master
    Conflicts:
    	web/index.xhtml

--- src/main/java/net/joastbg/sampleapp/dao/ClientDao.java
+++ src/main/java/net/joastbg/sampleapp/dao/ClientDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import net.joastbg.sampleapp.entities.CompteBancaire;
 import net.joastbg.sampleapp.exceptions.DaoException;
 import org.hibernate.Query;
 
